0f96576f20e84e050e8a9fe76cec3e47201a194a
Hineill <hineill.cespedes@pucp.edu.pe>
2023-11-20 23:00:43 -0500

    poniendo filter

--- src/main/java/com/example/proyectouwu/Filters/logOutFilter.java
+++ src/main/java/com/example/proyectouwu/Filters/logOutFilter.java
@@ -1,16 +1,36 @@
 package com.example.proyectouwu.Filters;
 
+import com.example.proyectouwu.Beans.Usuario;
 import jakarta.servlet.*;
 import jakarta.servlet.annotation.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 
-@WebFilter(filterName = "logOutFilter")
+@WebFilter(filterName = "logOutFilter" , servletNames = {"AnaliticasServlet" , "EventoServlet" , "Imagen", "ImagenActividadServlet" , "ImagenEventoServlet","ImagenUsuarioServlet", "ListaDeActividadesServlet" ,"ListaDeEventosServlet", "ListaDeUsuariosServlet", "MiCuentaServlet" , "MisDonacionesServlet" , "MisEventosServlet" , "NotificacionesServlet",    })
 public class logOutFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
-        chain.doFilter(request, response);
+
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse resp = (HttpServletResponse) response;
+
+        HttpSession session = req.getSession();
+        Usuario user = (Usuario) session.getAttribute("usuario");
+
+        if (user == null) {
+            resp.sendRedirect("InicioSesionServlet");
+        } else {
+            resp.setHeader("Cache-Control", "no-cache, no-store , must-revalidate");
+            resp.setHeader("Pragma", "no-cache");
+            resp.setDateHeader("Expires",0);
+
+            chain.doFilter(req, resp);
+        }
+
     }
 }
 

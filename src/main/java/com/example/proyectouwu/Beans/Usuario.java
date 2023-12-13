e4da844067b414a1b41d559b9a626b91be063502
May3012 <139161004+May3012@users.noreply.github.com>
2023-12-13 11:39:23 -0500

    finalizimoooo

--- src/main/java/com/example/proyectouwu/Beans/Usuario.java
+++ src/main/java/com/example/proyectouwu/Beans/Usuario.java
@@ -158,7 +158,7 @@ public class Usuario {
     public void setRecibioKit(boolean recibioKit) {
         this.recibioKit = recibioKit;
     }
-    public static String  ip = "35.193.29.250";
+    public static String  ip = "34.136.47.112";
     public static String nombreProyecto="SemanaDeIngenieria" ;
     //Metodo extra
     public void enviarCorreo(String idValidacion){
@@ -188,14 +188,14 @@ public class Usuario {
                 case"enviarLinkACorreo":
                     link = ip+":8080/"+ nombreProyecto +"/RegistroServlet?idCorreoValidacion=" + validacion.getIdCorreoValidacion() + "&codigoValidacion256=" + validacion.getCodigoValidacion256();
                     message.setSubject("Verificación de correo electrónico durante registro - Siempre Fibra");
-                    message.setText("¡Continua con tu registro! Haz clic en el siguiente link y completa tus datos : "+  link +"\n\nRecuerde que el uso de este link es único por solicitud de registro\n\nSiempre Fibra");
+                    message.setText("¡Continua con tu registro! Haz clic en el siguiente link y completa tus datos: \n" +  link +"\n\nRecuerde que el uso de este link es único por solicitud de registro\n\nSiempre Fibra\nMesa Directiva 2023");
                     Transport.send(message);
                     System.out.println("Correo enviado con éxito.");
                     break;
                 case "recuperarContrasena":
                     link = ip+":8080/"+ nombreProyecto +"/RecuperarContrasenaSegundoCasoServlet?idCorreoValidacion=" + validacion.getIdCorreoValidacion() + "&codigoValidacion256=" + validacion.getCodigoValidacion256();
                     message.setSubject("Solicitud de recuperación de contraseña - Siempre Fibra ");
-                    message.setText("¡Continúa con el proceso de recuperación de contraseña! Haz clic en el siguiente link e ingrese su nueva contraseña:"+ link +"\n\nSiempre Fibra");
+                    message.setText("¡Continúa con el proceso de recuperación de contraseña! Haz clic en el siguiente link e ingrese su nueva contraseña:\n"+ link +"\n\nSiempre Fibra\nMesa Directiva 2023");
                     Transport.send(message);
                     System.out.println("Correo enviado con éxito.");
                     break;
@@ -203,7 +203,7 @@ public class Usuario {
                     String fecha = obtenerFechaManana() ;
                     message.setSubject("Te ganaste el kit teleco - Siempre Fibra");
                     message.setText("¡Felicidades! Demostraste ser un real seguidor de la fibra con tu constante apoyo. Por tal motivo, queremos recompensarte con un kit teleco. \n" +
-                            "Podrás recoger tu kit en la bati (primer piso del V) el día : "+ fecha + " a las 3pm.\n\nEn caso no sea posible su asistencia tal día, responda el correo indicando sus fechas tentativas.\n\nSiempre Fibra");
+                            "Podrás recoger tu kit en la bati (primer piso del V) el día : "+ fecha + " a las 3pm.\n\nEn caso no sea posible su asistencia tal día, responda el correo indicando sus fechas tentativas.\n\nSiempre Fibra\nMesa Directiva 2023");
                     Transport.send(message);
                     System.out.println("Correo enviado con éxito.");
                     break;
@@ -234,7 +234,7 @@ public class Usuario {
             String link = ip+":8080/"+nombreProyecto + "/InicioSesionServlet";
             message.setSubject("Proceso de registro completado - Siempre Fibra");
             message.setText("¡Bienvenido a la plataforma web de la Fibra!\nAquí podrás observar los resultados y la gestión de todos los eventos durante la realización de las actividades.\n\nEsperamos su participación activa durante Semana de Ingeniería." +
-                    " Dale clic aquí para continuar : " +  link +"\n\nSiempre Fibra");
+                    " Dale clic aquí para continuar: \n" +  link +"\n\nSiempre Fibra\nMesa Directiva 2023");
             Transport.send(message);
             System.out.println("Correo enviado con éxito.");
 
@@ -263,7 +263,7 @@ public class Usuario {
             message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(destinatario));
             String link = ip+":8080/" +nombreProyecto +"/InicioSesionServlet";
             message.setSubject("Proceso de registro rechazado - Siempre Fibra");
-            message.setText("Lamentamos informate que no podemos aceptar tu registro a la página web de la fibra por el siguiente motivo: " +  motivo +"\n\nRecuerde que puede realizar el proceso de registro nuevamente.\nEsperamos su comprensión.\n\nSiempre Fibra");
+            message.setText("Lamentamos informate que no podemos aceptar tu registro a la página web de la fibra por el siguiente motivo: " +  motivo +"\n\nRecuerde que puede realizar el proceso de registro nuevamente.\nEsperamos su comprensión.\n\nSiempre Fibra\nMesa Directiva 2023");
             Transport.send(message);
             System.out.println("Correo enviado con éxito.");
 
@@ -323,13 +323,13 @@ public class Usuario {
             switch(tipoDeApoyo){
                 case"Barra":
                     message.setSubject("Solicitud de apoyo revisada en el evento de "+evento.getTitulo()+" - Siempre Fibra");
-                    message.setText("Se aceptó su participación en el evento de " + evento.getTitulo()+".\n\nUsted fue elegido para participar en la barra.\n\nRecuerde seguir las indicaciones del delegado de actividad correspondiente.\nNos vemos en la cancha.\n\nSiempre Fibra");
+                    message.setText("Se aceptó su participación en el evento de " + evento.getTitulo()+".\n\nUsted fue elegido para participar en la barra.\n\nRecuerde seguir las indicaciones del delegado de actividad correspondiente.\nNos vemos en la cancha.\n\nSiempre Fibra\nMesa Directiva 2023");
                     Transport.send(message);
                     System.out.println("Correo enviado con éxito.");
                     break;
                 case "Jugador":
                     message.setSubject("Aceptación de participación en Semana de Ingeniería - Siempre Fibra");
-                    message.setText("Se aceptó su participación en el evento de " + evento.getTitulo()+".\n\nUsted fue elegido para participar en el equipo.\n\nRecuerde seguir las indicaciones del delegado de actividad correspondiente.\nNos vemos en la cancha.\n\nSiempre Fibra");
+                    message.setText("Se aceptó su participación en el evento de " + evento.getTitulo()+".\n\nUsted fue elegido para participar en el equipo.\n\nRecuerde seguir las indicaciones del delegado de actividad correspondiente.\nNos vemos en la cancha.\n\nSiempre Fibra\nMesa Directiva 2023");
                     Transport.send(message);
                     System.out.println("Correo enviado con éxito.");
                     break;

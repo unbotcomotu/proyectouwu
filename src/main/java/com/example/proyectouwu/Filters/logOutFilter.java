ca4dd8e3dff531469a93e5cc9161da662644fc32
alex-segovia <a20213849@pucp.edu.pe>
2023-12-12 22:15:59 -0500

    validación de usuario baneado en el filter

--- src/main/java/com/example/proyectouwu/Filters/logOutFilter.java
+++ src/main/java/com/example/proyectouwu/Filters/logOutFilter.java
@@ -1,6 +1,7 @@
 package com.example.proyectouwu.Filters;
 
 import com.example.proyectouwu.Beans.Usuario;
+import com.example.proyectouwu.Daos.DaoBan;
 import com.example.proyectouwu.Daos.DaoUsuario;
 import jakarta.servlet.*;
 import jakarta.servlet.annotation.*;
@@ -10,7 +11,7 @@ import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 
-@WebFilter(filterName = "logOutFilter" , servletNames = {"AnaliticasServlet" , "EventoServlet" , "Imagen", "ImagenActividadServlet" , "ImagenEventoServlet","ImagenUsuarioServlet", "ListaDeActividadesServlet" ,"ListaDeEventosServlet", "ListaDeUsuariosServlet", "MiCuentaServlet" , "MisDonacionesServlet" , "MisEventosServlet" , "NotificacionesServlet"   })
+@WebFilter(filterName = "logOutFilter" , servletNames = {"AnaliticasServlet" , "EventoServlet" , "Imagen", "ImagenActividadServlet" , "ImagenEventoServlet","ImagenUsuarioServlet", "ListaDeActividadesServlet" ,"ListaDeEventosServlet", "ListaDeUsuariosServlet", "MiCuentaServlet" , "MisDonacionesServlet" , "MisEventosServlet" , "NotificacionesServlet","PaginaNoExisteServlet"})
 public class logOutFilter implements Filter {
 
     @Override
@@ -21,15 +22,20 @@ public class logOutFilter implements Filter {
 
         HttpSession session = req.getSession();
         Usuario user = (Usuario) session.getAttribute("usuario");
+        DaoBan daoBan = new DaoBan();
 
         if (user == null) {
             resp.sendRedirect("InicioSesionServlet");
         } else {
-            //req.getSession().setAttribute("usuario", new DaoUsuario().getUsuarioPorId(user.getIdUsuario()));
-            resp.setHeader("Cache-Control", "no-cache, no-store , must-revalidate");
-            resp.setHeader("Pragma", "no-cache");
-            resp.setDateHeader("Expires",0);
-            chain.doFilter(req, resp);
+            if(daoBan.usuarioBaneadoPorId(user.getIdUsuario())){
+                resp.sendRedirect("InicioSesionServlet");
+            }else {
+                //req.getSession().setAttribute("usuario", new DaoUsuario().getUsuarioPorId(user.getIdUsuario()));
+                resp.setHeader("Cache-Control", "no-cache, no-store , must-revalidate");
+                resp.setHeader("Pragma", "no-cache");
+                resp.setDateHeader("Expires", 0);
+                chain.doFilter(req, resp);
+            }
         }
 
     }
